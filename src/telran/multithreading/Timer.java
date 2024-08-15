68f2b10343b18ab0381b220e1ba0f992dc19cb58
Yuragranovsky <yuragranovsky@gmail.com>
2024-08-15 18:25:59 +0300

    HW #45 solution

--- src/telran/multithreading/Timer.java
+++ src/telran/multithreading/Timer.java
@@ -1,22 +1,34 @@
 package telran.multithreading;
 
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
 public class Timer extends Thread {
-//TODO displaying time in a given format and a given resolution
-	//example displaying each second, or each 5 seconds, etc.
-	public Timer() {
+private static final String DEFAULT_FORMATTER_PATTERN = "HH.mm.ss";
+private static final long DEFAULT_TIMER_RESOLUTION = 1000;
+private volatile boolean running = true;
+private DateTimeFormatter dtf;
+private long timerResolution;
+	public Timer(String pattern, long timerResolution) {
+		dtf = DateTimeFormatter.ofPattern(pattern);
+		this.timerResolution = timerResolution;
 		setDaemon(true);
 	}
+	public Timer() {
+		this(DEFAULT_FORMATTER_PATTERN, DEFAULT_TIMER_RESOLUTION);
+	}
 	public void run() {
-		while(true) {
-			System.out.println(LocalTime.now());
+		while(running) {
+			System.out.println(LocalTime.now().format(dtf));
 			try {
-				sleep(1000);
+				sleep(timerResolution);
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
+	}
+	public void finish() {
+		running = false;
 	}
 }
--- src/telran/multithreading/TimerControllerAppl.java
+++ src/telran/multithreading/TimerControllerAppl.java
@@ -5,11 +5,9 @@ public class TimerControllerAppl {
 	public static void main(String[] args) throws InterruptedException {
 		Timer timer = new Timer();
 		timer.start();
-		Thread.sleep(10000);
-		//TODO
-		//figure out a solution allowing timer stop
-		//as example in 5 seconds you stop the timer
-		//following 5 seconds application is running with no timer
+		Thread.sleep(5000);
+		timer.finish();
+		Thread.sleep(5000);
 
 	}
 
